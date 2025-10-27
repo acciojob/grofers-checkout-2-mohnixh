69e31b4836211a8d4ca2f96437b332a7beecf767
mohnixh <153740400+mohnixh@users.noreply.github.com>
2025-10-27 12:51:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+
+const rows = document.querySelectorAll("tr");
+
+let total = 0;
+
+for (let i = 1; i < rows.length; i++) {
+    const priceCell = rows[i].querySelector("td:nth-child(2)");
+    total += Number(priceCell.innerText);
+}
+
+const newRow = document.createElement("tr");
+
+const totalCell = document.createElement("td");
+totalCell.innerText = "Total: " + total + " Rs";
+totalCell.colSpan = 2; 
+
+newRow.appendChild(totalCell);
+
+const table = document.querySelector("table");
+table.appendChild(newRow);
